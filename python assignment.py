02e5c4b48a461cc7acc8f8f08fb8ac2fe26815a8
melvin3113 <55142406+melvin3113@users.noreply.github.com>
2020-07-21 22:32:34 +0530

    day 2

---
+++ python assignment.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[3]:
+
+
+print("hello       python")
+
+
+# In[6]:
+
+
+print(""" ()()()()()(~~~~"") """)
+
+
+# In[7]:
+
+
+print("hy\thello")
+
+
+# In[8]:
+
+
+print("hy\nhello")
+
+
+# In[9]:
+
+
+name="mel"
+age="20"
+print("my name is",name,"and of age",age)
+
+
+# In[11]:
+
+
+print(f"my name is {name} of age { age}")
+
+
+# In[12]:
+
+
+a=10
+b=10
+c=a+b
+print(c)
+
+
+# In[13]:
+
+
+b=2
+d=a**b
+print(d)
+
+
+# In[14]:
+
+
+print(a-b,a/b,a*b,a//b)
+
+
+# In[19]:
+
+
+a==b,a<=b,a>b,a!=b
+
+
+# In[20]:
+
+
+a|b
+
+
+# In[22]:
+
+
+a=16
+b=1
+a|b
+
+
+# In[23]:
+
+
+bin(a)
+
+
+# In[24]:
+
+
+a>b or b>a
+
+
+# In[25]:
+
+
+a>b and b>a
+
+
+# In[28]:
+
+
+x="abababab"
+"a" not in x
+
+
+# In[29]:
+
+
+5+10/2*2
+
+
+# In[31]:
+
+
+(5+(10/2))*2
+
+
+# In[ ]:
+
+
+
+
